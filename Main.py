5fbe6d5bbaf373df67c5a23666b0fdd9f71d41d7
Mohamed Abdelhakeem <mohamed.abdelhakeemx@gmail.com>
2025-03-04 15:07:49 +0200

    bugs fixed

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template , redirect, url_for, flash
 from forms import RegisterForm, LoginForm
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '124pofds 12h413kn f13pomo5'
+app.config['SECRET_KEY'] = '124pofds12h413knf13pomo5'
 
 @app.route('/')
 @app.route("/Home")
@@ -26,18 +26,18 @@ def login():
    form = LoginForm()
    if form.validate_on_submit():
          flash('Login Successfully', 'success')
-         return redirect(url_for('home'))
+         return redirect(url_for('Home'))
             
-   return render_template('login.html', title="Login", form=form, cssFile = "Static/css/Login.css")
+   return render_template('login.HTML', title="Login", form=form, cssFile = "Static/css/Login.css")
 
 @app.route('/register', methods=['GET','POST'])
 def register():
    form = RegisterForm()
    if form.validate_on_submit():
       flash('Account Created', 'success')
-      return redirect(url_for('Home'))
+      return redirect(url_for('login'))
    
-   return render_template('signup.html', title="Sign up",form=form, cssFile = "Static/css/signup.css")
+   return render_template('signup.HTML', title="Sign-Up",form=form, cssFile = "Static/css/signup.css")
 
 if __name__ == "__main__":
     app.run(debug=True,port=3000)
